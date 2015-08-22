05bac26972c2d35b97b3ae1007b9c3eb19126a43
Clemente Estrada <mirdrack@gmail.com>
2015-08-21 23:10:20 -0500

    Create the config folder and clean up the test restricted zone

---
+++ app/config/appConfig.js
@@ -0,0 +1,75 @@
+alinaApp.config(function ($routeProvider, $httpProvider) {
+
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'mainController'
+		})
+		.when('/home', {
+			templateUrl: 'pages/home.html',
+			controller: 'mainController'
+		})
+		.when('/about', {
+			templateUrl: 'pages/about.html',
+			controller: 'aboutController'
+		})
+		.when('/contact', {
+			templateUrl: 'pages/contact.html',
+			controller: 'contactController'
+		})
+		.when('/signup', {
+			templateUrl: 'pages/signup.html',
+			controller: 'sessionController'
+		})
+		.when('/signin', {
+			templateUrl: 'pages/signin.html',
+			controller: 'sessionController'
+		})
+		.when('/restricted', {
+			templateUrl: 'pages/restricted.html',
+			controller: 'restrictedController'
+		})
+		.when('/profile', {
+			templateUrl: 'pages/users/profile.html',
+			controller: 'profileController'
+		})
+		;
+
+		$httpProvider.interceptors.push(['$q', '$location', '$window', function ($q, $location, $window) {
+
+			return {
+				'request': function (config) {
+
+					config.headers = config.headers || {};
+					if($window.localStorage['token']) {
+						config.headers.Authorization = 'Bearer ' + $window.localStorage['token'];
+					}
+					return config;
+				},
+
+				'responseError': function (response) {
+
+					if(response.status == 401 || response.status == 403) {
+						$location.path('/signin');
+					}
+					return $q.reject(response);
+				}
+			};
+		}]);
+
+})
+.constant('urls', {
+	BASE: 'http://rea.app',
+	BASE_API: 'http://api.rea.app/v1'
+})
+.run(function ($rootScope, $location, $window) {
+
+	$rootScope.$on('$routeChangeStart', function (event, next) {
+		if($window.localStorage['token'] == null) {
+			if(next.templateUrl === 'partials/restricted.html') {
+				$location.path('/signup');
+			}
+		}
+	});
+
+});
--- app/services/userService.js
+++ app/services/userService.js
@@ -2,6 +2,11 @@ alinaApp.factory('userService', ['$http', 'urls', '$window', function ($http, ur
 
 	return {
 
+		getUsers: function(success, error) {
+
+			$http.get(urls.BASE + '/user?token='+token).success(success).error(error);
+		},
+
 		getProfile: function(success, error) {
 
 			$http.get(urls.BASE + '/user/profile?token='+token).success(success).error(error);
