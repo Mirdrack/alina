27d4675298c5cdb7500c7875c87d0de77ff64908
Clemente Estrada <mirdrack@gmail.com>
2016-02-24 22:36:29 -0600

    Copied alarm button from station/show to station/alarms

--- app/controllers/stations/stationAlarmsController.js
+++ app/controllers/stations/stationAlarmsController.js
@@ -1,22 +1,34 @@
 alinaApp.controller('stationAlarmsController',
-function ($scope, $rootScope, stationAlarmService, stationService) {
+function ($scope, $rootScope, $routeParams, stationAlarmService, stationService, urls) {
 
 	$scope.pageClass = 'page-standard';
 
-	stationService.getStationsList(function (response) {
+	stationService.getStation(
+		function (response) {
 
-		$scope.stations = response.data;
-	},
-	function (response) {
+			$scope.station = response.data;
+			if($scope.station.alarm_activated == true) {
 
-		$rootScope.error = response.error;
-	});
+				$scope.alarmStatus = 'On';
+				$scope.btnAlarmLabel = 'Turn Alarm Off';
+			}
+			else {
+
+				$scope.alarmStatus = 'Off';
+				$scope.btnAlarmLabel = 'Turn Alarm On';
+			}
+		},
+		function (response){
+
+			$scope.error = response.error;
+		},
+		$routeParams.id
+	);
 
 	stationAlarmService.getStationAlarmsList(function (response) {
 
 		$scope.stationAlarms = response.data.station_alarms;
 		$scope.paginator = response.data.paginator;
-		console.log(response.data.paginator);
 	}, 
 	function (response) {
 
@@ -35,4 +47,68 @@ function ($scope, $rootScope, stationAlarmService, stationService) {
 			$rootScope.error = response.error;
 		}, page);
 	};
+
+	// We init the socket
+	var clientId = Math.floor((Math.random() * 10) + 1);
+	var socket = io(urls.BASE_NODE, { query: 'clientName=Alina-alarms-' + clientId });
+
+	socket.on('activate-alarm-server', function (data) {
+
+		$scope.btnAlarmLabel = 'Turn Alarm Off';
+		$scope.alarmStatus = 'On';
+		$scope.$apply();
+	});
+
+	socket.on('deactivate-alarm-server', function (data) {
+
+		$scope.btnAlarmLabel = 'Turn Alarm On';
+		$scope.alarmStatus = 'Off';
+		$scope.$apply();
+	});
+
+	socket.on('error-server', function (data) {
+
+		console.log(data);
+	});
+
+	$scope.changeAlarmStatus = function (id) {
+
+		console.log(id);
+		if($scope.alarmStatus == 'Off') {
+
+			var event = {
+				user_id: parseInt($user.id),
+				station_id: id,
+				event_type_id: 3,
+				ip_address: clientIp,
+			};
+
+			var data = {
+				event_type: 'alarm-activated',
+				message: 'Alarm has been activated',
+				event: event,
+			};	
+
+			socket.emit('activate-alarm', data);
+
+		}
+		if($scope.alarmStatus == 'On') {
+
+			var event = {
+				user_id: parseInt($user.id),
+				station_id: id,
+				event_type_id: 4,
+				ip_address: clientIp,
+			};
+
+			var data = {
+				event_type: 'alarm-deactivated',
+				message: 'Alarm has been activated',
+				event: event,
+			};
+
+			socket.emit('deactivate-alarm', data);
+		}
+	};
+
 });
