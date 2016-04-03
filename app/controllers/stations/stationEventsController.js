f58eb338c32f98733521e4968e121615f60ede5a
Clemente Estrada <mirdrack@gmail.com>
2016-04-02 19:50:13 -0600

    Added filter for event types

--- app/controllers/stations/stationEventsController.js
+++ app/controllers/stations/stationEventsController.js
@@ -38,9 +38,25 @@ function ($scope, $rootScope, $routeParams, stationEventService, stationService,
 		}, page);
 	};
 
-	console.log('stationEventsController');
-
-	
-
-
+	$scope.filter = {};
+	$scope.filter.eventOptions = [
+			{"value": 1, "label": "Encendido de pozo"},
+			{"value": 2, "label": "Apagado de pozo"},
+			{"value": 3, "label": "Encendido de alarma puerta pozo"},
+			{"value": 4, "label": "Apagado de alarma puerta pozo"},
+			{"value": 5, "label": "Encendido de alarma movimiento"},
+			{"value": 6, "label": "Apagado de alarma movimiento"},
+			{"value": 7, "label": "Encendido de alarma puerta irrigación"},
+			{"value": 8, "label": "Apagado de alarma puerta irrigación"},
+			{"value": 9, "label": "Encendido de alarma movimiento irrigación"},
+			{"value": 10, "label": "Apagado de alarma movimiento irrigación"},
+			{"value": 11, "label": "Puerta Abierta Cuarto Pozo"},
+			{"value": 12, "label": "Puerta Cerrada Cuarto Pozo"},
+			{"value": 13, "label": "Puerta Abierta Cuarto Riego"},
+			{"value": 14, "label": "Puerta Cerrado Cuarto Riego"},
+			{"value": 15, "label": "Movimiento Cuarto Pozo"},
+			{"value": 16, "label": "Movimiento Cuarto Riego"},
+			{"value": 17, "label": "Alarma Desactivada Manualmente"},
+			{"value": 18, "label": "Alarma Activada Manualmente"}
+		];
 });
