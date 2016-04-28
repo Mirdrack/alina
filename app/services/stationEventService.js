0319ca04f6d91b09835963f1e8c04eb523b84227
Clemente Estrada <mirdrack@gmail.com>
2016-04-27 20:21:15 -0500

    Changed filter for events table

--- app/services/stationEventService.js
+++ app/services/stationEventService.js
@@ -2,12 +2,20 @@ alinaApp.factory('stationEventService', ['$http', 'urls', function ($http, urls)
 
 	
 	return {
-		getStationEventsList: function (success, error, page) {
+		getStationEventsList: function (success, error, page, filter) {
 
-			if(page != null)
+			if(page != null) {
+
 				var url = urls.BASE + '/station-event?page=' + page;
-			else
+				if(filter != null)
+					url += '&filter=' + filter;
+			}
+			else {
+
 				var url = urls.BASE + '/station-event';
+				if(filter != null)
+					url += '?filter=' + filter;
+			}
 			$http.get(url).success(success).error(error);
 		}
 	};
